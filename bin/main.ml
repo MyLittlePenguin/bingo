894cfb90267b40dea9847aad89ccbf1b5f927fd3
Ronny Krone <ronny.krone@volkwagen-groupservices.com>
2025-09-10 13:54:02 +0200

    added interface definition

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,8 @@
-open Permutations.Per
+open Permutations
 
-let row1 = gen_list 10 |> bingo
-let row2 = gen_list 10 |> List.map ((+) 10) |> bingo 
-let row3 = gen_list 10 |> List.map ((+) 20) |> bingo 
+let row1 = Per.gen_list 10 |> Per.bingo
+let row2 = Per.gen_list 10 |> List.map ((+) 10) |> Per.bingo
+let row3 = Per.gen_list 10 |> List.map ((+) 20) |> Per.bingo
 let rows = row1 @ row2 @ row3
 
 let () = rows |> List.length |> Printf.printf "%d \n"
---
+++ lib/per.mli
@@ -0,0 +1,3 @@
+val gen_list : int -> int list
+val bingo: 'a list -> 'a list list
+val mutate: 'a list -> 'a list list
